edcc11df7fde45c941fa2558feeef0d74e5ad013
neeratipooja <116306537+neeratipooja@users.noreply.github.com>
2022-11-01 15:28:55 +0530

    Add files via upload

---
+++ w3school.py
@@ -0,0 +1,30 @@
+'''DATE=1st nov 2022
+DAY= monday
+TOPIC= W3 solving
+AUTHOR= pooja'''
+
+x=['hello','pooja']
+print(type(x)) #list
+x=[]
+print(type(x))
+x={'hello':'pooja'}
+print(type(x))  #dict
+x={}
+print(type(x))  #dict
+y={3}
+print(type(y)) #set
+y={'.'}
+print(type(y)) #set
+x=6
+x=complex(x)
+print(x)   #6+0j
+x='hey pooja'
+print(x[4]) #k
+print(len(x)) #9
+print(x.replace('k','H')) #hey Hiran
+print(bool('kiran')) #true
+print(bool('')) #false
+print(bool(0)) #false
+print(bool('.'))#true
+
+
